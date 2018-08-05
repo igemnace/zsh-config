c311b96c38db5520a6d71311410a1bfa13c95060
Ian Emnace <igemnace@gmail.com>
2018-08-06 03:38:56 +0800

    Clean up highlight.zsh
    Albeit the overall resulting prompt remains unchanged, highlight.zsh has
    been massively updated to be more in keeping with nicer shell scripting
    principles.
    
    Some of these changes include:
    
    - improvements to readability (case in point: the massive one-liner
      parameter expansion for collapse_pwd is now entirely refactored into
      an easy-to-follow algorithm)
    - less reliance on external tools and/or non-standard flags for such
      tools
    - better use of zsh idioms, notably parameter expansion flags and
      conditional prompt expressions
    - generous reduction of superfluous lines of code

--- cfg/.zsh/highlight.zsh
+++ cfg/.zsh/highlight.zsh
@@ -1,84 +1,82 @@
 #!/usr/bin/env zsh
 
 # cache newline in a variable
-newline=$'\n'
+nl=$'\n'
 
 ### CONTEXT
 ### prints directory
 collapse_pwd() {
-  if [[ $PWD == "$HOME" ]]; then
-    echo "%F{4}%B~%f%b"
-  elif [[ $PWD == "/" ]]; then
-    echo "%F{4}%B/%f%b"
+  local cwd dirs collapsed
+  cwd=${PWD/$HOME/\~}
+  dirs=("${(s:/:)cwd}") # break cwd into array of individual dirs
+  collapsed=()
+
+  # collapse each dir into just one letter
+  # except for the last one
+  for dir in "${(@)dirs[1,-2]}"; do
+    [[ -n "$dir" ]] && collapsed+=("${dir:0:1}")
+  done
+  # highlight last dir
+  collapsed+=("%F{4}%B${dirs[-1]}%b")
+
+  if [[ "$cwd" == / ]]; then
+    # case: cwd is root /
+    echo '%F{4}%B/%b'
+  elif [[ ${dirs[1]} == '~' ]]; then
+    # case: cwd starts with ~, simply print collapsed dirs
+    echo "%F{8}${(j:/:)collapsed}%f"
   else
-    echo ${${:-/${(j:/:)${(M)${(s:/:)${(D)PWD:h}}#(|.)[^.]}}/%F{4}%B${PWD:t}%b}//\/~/\~} | sed -e 's/\/\//\//'
+    # case: cwd is not under ~, prepend root /
+    echo "%F{8}/${(j:/:)collapsed}%f"
   fi
 }
-custom_prompt_context='%F{8}$(collapse_pwd)%f'
+custom_prompt_context='$(collapse_pwd)'
 
 ### GIT
 ### prints git branch and status
 ### but only if current dir is a git repo
 git_info() {
-  if git status --porcelain &>/dev/null; then
-    status_output=$(git status -b --porcelain)
-    stash_output=$(git stash list)
-    first_line=$(sed -n '1p' <<< "$status_output")
-    rest_lines=$(sed -n '2,$p' <<< "$status_output")
+  local status_output
+  status_output=$(git status -b --porcelain 2>/dev/null)
+  if ! (( $? )); then
+    local first_line rest_lines
+    first_line=$(head -n 1 <<< "$status_output")
+    rest_lines=$(tail -n +2 <<< "$status_output")
 
-    branch=$(sed -E 's/## (.*)(\.\.\..*|$)/\1/' <<< "$first_line")
-    ahead=$(sed -E 's/.*\[.*ahead (\w+).*/\1/' <<< "$first_line")
-    behind=$(sed -E 's/.*\[.*behind (\w+).*/\1/' <<< "$first_line")
+    # first line of status_output contains branch name and ahead/behind info
+    local branch ahead behind
+    [[ $first_line =~ '## (.*)(\.\.\..*|$)' ]] && branch=${match[1]}
+    [[ $first_line =~ '.*\[.*ahead (\w+).*' ]] && ahead=${match[1]}
+    [[ $first_line =~ '.*\[.*behind (\w+).*' ]] && behind=${match[1]}
+
+    # ahead/behind flags
+    local ahead_flag
+    local behind_flag
+    (( $ahead )) && ahead_flag="%F{6}+${ahead}%f"
+    (( $behind )) && behind_flag="%F{1}-${behind}%f"
+
+    # rest of status_output contain info on dirty files
+    local unstaged staged untracked
     unstaged=$(cut -c 2 <<< "$rest_lines" | tr -d ' \n?')
     staged=$(cut -c 1 <<< "$rest_lines" | tr -d ' \n?')
     untracked=$(cut -c 1 <<< "$rest_lines" | tr -d ' \n')
 
-    stashed=""
-    [[ -n $stash_output ]] && stashed=$(wc -l <<< "$stash_output")
-
-    track_flag=""
+    # combine dirty file info into a single track flag
+    local track_flag
     [[ -n $staged ]] && track_flag+="+"
     [[ -n $unstaged ]] && track_flag+="!"
     [[ $untracked == *\?* ]] && track_flag+="?"
-    if [[ -z $track_flag ]]; then
-      final_track_flag=""
-    else
-      final_track_flag="%F{3}${track_flag}%f"
-    fi
+    [[ -n $track_flag ]] && track_flag="%F{3}${track_flag}%f"
 
-    ahead_flag=""
-    [[ ! $ahead =~ "##" ]] && ahead_flag="$ahead"
-    if [[ -z $ahead_flag ]]; then
-      final_ahead_flag=""
-    else
-      final_ahead_flag="%F{6}+${ahead}%f"
-    fi
+    # git stash list contains stash info
+    local stash_flag
+    [[ -n $(git stash list) ]] && stash_flag="%F{2}s%f"
 
-    behind_flag=""
-    [[ ! $behind =~ "##" ]] && behind_flag="$behind"
-    if [[ -z $behind_flag ]]; then
-      final_behind_flag=""
-    else
-      final_behind_flag="%F{1}-${behind}%f"
-    fi
+    # combine all flags
+    local combined_flags="${track_flag}${stash_flag}${ahead_flag}${behind_flag}"
+    [[ -n $combined_flags ]] && combined_flags="%F{8}[%B${combined_flags}%b%F{8}]"
 
-    stash_flag=""
-    [[ $stashed != 0 ]] && stash_flag="$stashed"
-    if [[ -z $stash_flag ]]; then
-      final_stash_flag=""
-    else
-      final_stash_flag="%F{2}s%f"
-    fi
-
-    combined_flags="${final_track_flag}${final_stash_flag}${final_ahead_flag}${final_behind_flag}"
-    if [[ -z $combined_flags ]]; then
-      final_flags=""
-    else
-      final_flags="%F{8}[%B${combined_flags}%b%F{8}]"
-      # final_flags=$combined_flags
-    fi
-
-    echo "%F{8}git:%F{5}%B${branch}%b${final_flags}%f"
+    echo "%F{8}git:%F{5}%B${branch}%b${combined_flags}%f"
   fi
 }
 custom_prompt_git='$(git_info)'
@@ -87,26 +85,25 @@ custom_prompt_git='$(git_info)'
 ### prints node version
 ### but only if package.json exists in current directory
 node_info() {
-  if [[ -d "./node_modules" ]]; then
-    node_modules_flag=""
-  else
-    node_modules_flag="%F{8}%b[%F{3}%B!%b%F{8}]%f%b"
-  fi
-  if [[ -f "./package.json" ]]; then
-    echo "%F{8}node:%F{1}%B$(node --version | sed -e 's/v//')%f%b$node_modules_flag"
-  fi
+  [[ -f ./package.json ]] || return
+
+  # print alert if no node_modules dir exists
+  local node_modules_flag
+  ! [[ -d ./node_modules ]] && node_modules_flag="%F{8}%b[%F{3}%B!%b%F{8}]%f%b"
+
+  echo "%F{8}node:%F{1}%B$(node --version | sed -e 's/v//')%f%b$node_modules_flag"
 }
 custom_prompt_node='$(node_info)'
 
 ### SUFFIX
 ### prints input prompt
-custom_prompt_suffix='%F{8}%% %f'
+custom_prompt_suffix='%F{8}%%%f'
 
 ### EXIT
 ### prints last exit code
 ### but only if non-zero
 colored_exit_code() {
-  echo "%(?..${newline}%F{8}exit %F{1}%?)%f"
+  echo "%(?..${nl}%F{8}exit %F{1}%?)%f"
 }
 custom_prompt_exit='$(colored_exit_code)'
 
@@ -114,7 +111,7 @@ custom_prompt_exit='$(colored_exit_code)'
 ### prints background jobs
 ### but only if there exists at least one
 background_jobs() {
-  [[ -n $(jobs) ]] && echo "%F{4}bg %f"
+  echo "%1(j.%F{4}bg %f.)"
 }
 custom_prompt_jobs='$(background_jobs)'
 
@@ -122,10 +119,10 @@ custom_prompt_jobs='$(background_jobs)'
 ### prints username and hostname
 ### but only if logged in through ssh
 user_id() {
-  if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+  if [[ -n "$SSH_CLIENT" ]] || [[ -n "$SSH_TTY" ]]; then
     echo "%F{8}%n@%F{3}%B%m%b%F{8}: %f"
   fi
 }
 custom_prompt_id='$(user_id)'
 
-export PROMPT="${newline}${custom_prompt_id}${custom_prompt_context} ${custom_prompt_git} ${custom_prompt_node}${custom_prompt_exit}${newline}${custom_prompt_jobs}${custom_prompt_suffix}%F{7}"
+export PROMPT="${nl}${custom_prompt_id}${custom_prompt_context} ${custom_prompt_git} ${custom_prompt_node}${custom_prompt_exit}${nl}${custom_prompt_jobs}${custom_prompt_suffix} %F{7}"
